47a586632ec6b45e3d6f96de91840bf955e9388f
Pol Miro <polmiro@gmail.com>
2013-12-06 16:40:53 -0800

    Clean up query with WITH clause

--- lib/periodic_calculations/query.rb
+++ lib/periodic_calculations/query.rb
@@ -14,23 +14,22 @@ module PeriodicCalculations
     # @return [Array<Array>] for each period the time of the interval and the count of it
     def initialize(relation, query_options)
       @relation = relation
-      @interval_unit = query_options.interval_unit
+      @operation = query_options.operation.upcase
       @column_name = query_options.column_name
       @window_function = window_function(query_options)
-      @operation = query_options.operation.upcase
       @binds = {
-        :interval_unit   => query_options.interval_unit,
-        :interval        => "1 #{query_options.interval_unit.upcase}",
-        :window_start    => query_options.window_start,
-        :window_end      => query_options.window_end,
-        :timezone_offset => "#{query_options.timezone_offset} seconds"
+        :unit     => query_options.interval_unit,
+        :interval => "1 #{query_options.interval_unit.upcase}",
+        :start    => query_options.window_start,
+        :end      => query_options.window_end,
+        :offset   => "#{query_options.timezone_offset} seconds"
       }
     end
 
     def execute
       ActiveRecord::Base.connection_pool.with_connection do |connection|
         connection.execute(sanitized_sql).map do |elem|
-          [Date.parse(elem["frame"]).to_time, elem["frame_result"].to_i]
+          [Date.parse(elem["frame"]).to_time, elem["result"].to_i]
         end
       end
     end
@@ -56,35 +55,55 @@ module PeriodicCalculations
       #    Cannot be done before in the same query because window function iterates after the
       #    where/group by/having clauses (could be improved for non cumulative queries)
 
-      sanitized_result_set_sql = @relation.select("date_trunc('#{@interval_unit}', activities.created_at + INTERVAL :timezone_offset) as frame, #{@operation}(#{@column_name}) AS frame_result").to_sql
+      select_sql = <<-SQL
+        date_trunc(:unit, activities.created_at + INTERVAL :offset) AS frame,
+        #{@operation}(#{@column_name})                              AS result
+      SQL
+
+      frame_and_result_select = @relation.select(select_sql).to_sql
 
       <<-SQL
-        SELECT frame, frame_result
-        FROM
-        (
-          SELECT DISTINCT frame,
-                 #{@operation}(frame_result) OVER (#{@window_function} BY frame) AS frame_result
-          FROM
-          (
-            #{sanitized_result_set_sql}
+        WITH
+          -- generate series within window
+          -- (with shifted timezones utc_date -> zone_date)
+          grid AS (
+            SELECT  date_trunc(:unit, serie) AS frame,
+                    NULL::integer            AS result
+            FROM    generate_series(
+                      :start::timestamp + INTERVAL :offset,
+                      :end::timestamp + INTERVAL :offset,
+                      :interval
+                    )                                 AS serie
+          )
 
-            -- group results by date (with shifted timezones utc_date -> zone_date)
-            GROUP BY date_trunc(:interval_unit, activities.created_at + INTERVAL :timezone_offset)
+          -- preprocess results grouping by interval
+          -- (with shifted timezones utc_date -> zone_date)
+          , preprocessed_results AS (
+            #{frame_and_result_select}
+            GROUP BY date_trunc(:unit, activities.created_at + INTERVAL :offset)
+          )
 
-            UNION ALL
+          -- running window function calculate results and fill up gaps
+          , results AS (
+            SELECT  DISTINCT frame,
+                    #{@operation}(result) OVER (#{@window_function} BY frame) AS result
+            FROM (
+              SELECT frame, result FROM preprocessed_results
+              UNION ALL
+              SELECT frame, result FROM grid
+            ) AS fulfilled_gaps
+          )
 
-            -- generate series within window (with shifted timezones utc_date -> zone_date)
-            SELECT date_trunc(:interval_unit, serie) AS frame, NULL AS frame_result
-            FROM generate_series(:window_start::timestamp + INTERVAL :timezone_offset, :window_end::timestamp + INTERVAL :timezone_offset, :interval) AS serie
-          ) AS all_dates_results
-        ) AS unbounded_results
-        WHERE
-          -- cut down with shifted input dates (utc date -> zone date)
-          frame BETWEEN
-            date_trunc(:interval_unit, :window_start::timestamp + INTERVAL :timezone_offset)
-            AND
-            date_trunc(:interval_unit, :window_end::timestamp + INTERVAL :timezone_offset)
-        ORDER BY frame
+        -- cut out values outside window (needed here if cumulative, room for improvement)
+        -- (with shifted timezones utc_date -> zone_date)
+        SELECT    frame,
+                  result
+        FROM      results
+        WHERE     frame BETWEEN
+                    date_trunc(:unit, :start::timestamp + INTERVAL :offset)
+                    AND
+                    date_trunc(:unit, :end::timestamp + INTERVAL :offset)
+        ORDER BY  frame ASC
       SQL
     end
 
