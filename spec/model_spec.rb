cafe186526b68c9cde5648d3e1d8e660838e2340
Pol Miro <polmiro@gmail.com>
2013-12-06 16:40:53 -0800

    Set up postgres in dummy test app

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -1,5 +1,3 @@
-require 'spec_helper'
-
 describe TestModel do
 
   before { clean_database }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,27 +1,6 @@
 require 'coveralls'
 Coveralls.wear!
 
-$LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
-
-require 'rails/all'
+ENV["RAILS_ENV"] = "test"
+require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
-require 'count_during'
-
-ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ":memory:")
-
-ActiveRecord::Schema.define(version: 1) do
-  create_table :test_models do |t|
-    t.integer :number
-    t.timestamps
-  end
-end
-
-class TestModel < ActiveRecord::Base
-end
-
-def clean_database
-  models = [TestModel]
-  models.each do |model|
-    ActiveRecord::Base.connection.execute "DELETE FROM #{model.table_name}"
-  end
-end
