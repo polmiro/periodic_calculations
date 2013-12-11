325b1661c23740da9d6dfd1e96f278a7676d3059
Pol Miro <polmiro@gmail.com>
2013-12-11 15:33:11 -0800

    Move performance_matchers to support directory

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ Coveralls.wear!
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
-require 'performance_matchers'
+require 'support/performance_matchers'
 
 RSpec.configure do |config|
   config.filter_run_excluding :performance => true
